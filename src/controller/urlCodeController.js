f97b391ebf94a8813c5d2de36919f992d1bb5af8
ajit kerle <ajitkerle70@gmail.com>
2022-07-18 20:43:48 +0530

    minor change

--- src/controller/urlCodeController.js
+++ src/controller/urlCodeController.js
@@ -2,23 +2,37 @@ const urlCodeModel = require('../model/urlCodeModel');
 const validUrl = require('valid-url');
 const shortid = require('shortid')
 
-const createUrlCode = async function(req,res){
-        try{
-            let data = req.body;
-            if (Object.keys(data).length==0) return res.status(400).send({status: false, data: "Body can not be empty"})
+//======<<<<< regex declaration >>>>>>========
+// const validUrl = /^(http(s)?:\/\/)[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?#[\]@!\$&'\(\)\*\+,;=.]+$/
 
-            let shortener = await urlCodeModel.create(data);
-            let result = {
-                longUrl: shortener.longUrl,
-                shortUrl: shortener.shortUrl,
-                urlCode: shortener.urlCode
+
+const createUrlCode = async function (req, res) {
+    try {
+        let data = req.body;
+
+        if (Object.keys(data).length == 0) {
+            return res.status(400).send({ status: false, data: "Body can not be empty" })
+        } else {
+
+            if (validUrl.isUri(data.longUrl)) {
+                // let baseUrl="http://localhost:3000/" 
+                 
+                let shortener = await urlCodeModel.create(data);
+                let result = {
+                    longUrl: shortener.longUrl,
+                    shortUrl: shortener.shortUrl,
+                    urlCode: shortener.urlCode
+                }
+                return res.status(201).send({ status: true, data: result })
+            } else {
+            return res.status(400).send({ status: false, data: "Provided url format is wrong " })
             }
-           return res.status(201).send({status: true, data: result})
-
-        }catch(error){
-            res.status(500).send({status: false, error: error.message})
         }
-        
+
+    } catch (error) {
+        res.status(500).send({ status: false, error: error.message })
+    }
+
 }
 
-module.exports = {createUrlCode}
+module.exports = { createUrlCode }
