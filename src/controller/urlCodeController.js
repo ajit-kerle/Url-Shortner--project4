051b056421148182f0ffc56827e7abbb3302fddd
functionup-am <akshaymakwana350@gmail.com>
2022-07-18 15:40:07 +0530

    project-4

--- src/controller/urlCodeController.js
+++ src/controller/urlCodeController.js
@@ -1,20 +1,20 @@
 const urlCodeModel = require('../model/urlCodeModel');
 const validUrl = require('valid-url');
-const shortUrl = require('node-url-shortener');
-
-// shortUrl.short( function (err, url) );
+const shortid = require('shortid')
 
 const createUrlCode = async function(req,res){
         try{
             let data = req.body;
-            const {longUrl} = data;
+            if (Object.keys(data).length==0) return res.status(400).send({status: false, data: "Body can not be empty"})
 
-            let saveddata = await urlCodeModel.create(data);
-            res.status(201).send({status: true, data: saveddata})
-            
-            
-           
-   
+            let shortener = await urlCodeModel.create(data);
+            let result = {
+                longUrl: shortener.longUrl,
+                shortUrl: shortener.shortUrl,
+                urlCode: shortener.urlCode
+            }
+           return res.status(201).send({status: true, data: result})
+
         }catch(error){
             res.status(500).send({status: false, error: error.message})
         }
